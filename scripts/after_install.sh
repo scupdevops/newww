e7b98f44b4095391aeaec3a4518161c4d7fd411e
root <root@app-32748.scup.com>
2015-10-28 08:25:29 +0000

    add npm install in after_install and remove npm install before_install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -11,4 +11,22 @@ sed -i "/license_key: 'license key here',/c\license_key: '16a02e8939511e498f7497
 sed -i "/app_name: \['My Application'\],/c\app_name: \['newww'\]," newrelic.js
 cp /var/www/newww/env /var/www/newww/.env
 cp /var/www/newww/env.example /var/www/newww/.env.example
+cd /tmp
+wget http://download.redis.io/releases/redis-3.0.5.tar.gz
+tar xzf redis-3.0.5.tar.gz
+cd redis-3.0.5
+make -j 9
+./src/redis-server &
+npm install forever -g
+npm install -g gulp
+npm install gulp-util
+npm install pretty-hrtime
+npm install chalk
+npm install semver
+npm install archy
+npm install liftoff
+npm install tildify
+npm install interpret
+npm install v8flags
+npm install gulp
 chown -R deployer:deployer /var/www/newww
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,16 +8,4 @@ echo "172.30.4.165 puppetmaster.scup.com puppetmaster" >> /etc/hosts
 yum groupinstall -y 'Development Tools'
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs puppet
-npm install forever -g
-npm install -g gulp
-npm install gulp-util
-npm install pretty-hrtime
-npm install chalk
-npm install semver
-npm install archy 
-npm install liftoff
-npm install tildify
-npm install interpret
-npm install v8flags
-npm install gulp
 echo "* * * * * root puppet agent --test --server puppetmaster.scup.com" >> /etc/cron.d/puppet
