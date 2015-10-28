4beaf215bb4ebd6cf6d4c363ba833493702ac202
root <root@app-32748.scup.com>
2015-10-28 09:00:05 +0000

    Add redis boot script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -15,6 +15,7 @@ cd /tmp
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-6.rpm
 yum --enablerepo=remi,remi-test install redis 
+/etc/init.d/redis start
 npm install forever -g
 npm install -g gulp
 npm install gulp-util
